64b3300c887de575ffb383cb6fdb04bc8144320d
sleeping-anonymous <157833279+sleeping-anonymous@users.noreply.github.com>
2026-01-05 18:28:10 +0000

    Show the form values

--- script.js
+++ script.js
@@ -41,18 +41,41 @@ function render() {
         bookRead.textContent = book.read;
         bookRead.classList.add("read-status");
         bookCard.appendChild(bookRead);
+
+        /*  bookCard.innerHTML = `
+            <h3 class="title">${book.title}</h3>
+            <p class="author">${book.author}</p>
+            <p class="pages">${book.pages} pages</p>
+            <p class="read-status">${book.read ? "Read" : "Not Read"}</p>
+        `; 
+        one way to do same */
+
+        lib.appendChild(bookCard);
     }
 }
 
 let btnAdd = document.querySelector(".add-book");
-const modal = document.querySelector("#dialog");
-const confirmBtn = document.querySelector("#confirm-btn");
+const dialog = document.querySelector("#dialog");
+const form = document.querySelector("form");
 
 btnAdd.addEventListener("click", () => {
     dialog.showModal();
 })
 
-confirmBtn.addEventListener("click", (e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
+
+    const formData = new FormData(form);  // browser provided API that reads values from form using "name" attribute
+
+    const title = formData.get("title");
+    const author = formData.get("author");
+    const pages = Number(formData.get("pages"));  // FormData return strings(or null)
+    const read = formData.get("read-status") === "read";  // stores true or false where "read" is radio value inf orm
+
+    addBookToLibrary(title, author, pages, read);
+    render();
+
+    form.reset();
     dialog.close();
+
 })
